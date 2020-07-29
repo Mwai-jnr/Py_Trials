fdea9885c44389e5d9ae0b48127f2913a19dbbc8
vcom254 <mwaivictor07@yahoo.com>
2020-07-29 19:08:29 +0300

    Lesson 20

---
+++ L19.py
@@ -0,0 +1,11 @@
+# lesson 20.
+# Adding and removing items in a list.
+
+fruit_list = ['Banana', 'grapes', 'lemon', 'Pineapple', 'apple']
+num_list = [1,2,3,4,5,6,7]
+
+fruit_list.append('Oranges')
+
+del fruit_list [1]
+
+print(num_list, fruit_list)
