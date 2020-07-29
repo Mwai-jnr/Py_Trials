acb12d6b17575d4e3b2e796aed8fb7e06b13672a
vcom254 <mwaivictor07@yahoo.com>
2020-07-29 12:42:22 +0300

    lesson 18

---
+++ l18.py
@@ -0,0 +1,9 @@
+#stirng arithmetics
+
+space =' ' * 30
+
+print ('%s space before the text' % space)
+print ('%s space before the text' % space)
+print ('%s space before the text' % space)
+print  ('This one is for you')
+print (23)
