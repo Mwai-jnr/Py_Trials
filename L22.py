d2bf8c1a91bf7acedf8cfbbdb1ef09c084cc66ea
vcom254 <mwaivictor07@yahoo.com>
2020-08-04 11:46:57 +0300

    lesson 22

---
+++ L22.py
@@ -0,0 +1,19 @@
+# combining data from two lists
+
+fruit_list = ['Banana', 'PineApple', 'Tumeric']
+num_list = [1,2,3,4,5,6]
+num_fruit_list = fruit_list + num_list
+print(num_fruit_list)
+
+# multiplying lists.
+
+list_num = [1,2,3]
+multi_list = list_num * 5
+print(multi_list)
+
+# Tuples.
+# Tuples use parenthesis  unlike lis which use square brackets.
+num = (1,2,3,4,5,6)
+
+print(num)
+print(num[2])
