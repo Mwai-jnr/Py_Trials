d58462855d1150996cec7ba799a43a4b64a5dd70
vcom254 <mwaivictor07@yahoo.com>
2020-08-07 21:18:20 +0300

    lesson 47

---
+++ L47.py
@@ -0,0 +1,35 @@
+#functions
+
+#example
+# defining a function.
+#def invite():
+ #   print('you have jus been invited to visit')
+    
+#print('am outside the function')
+
+#calling the function
+
+#invite()
+#print('am also outside the function')
+
+#First Function.
+
+#def hello(fname, lname):
+  #  print ('hello %s %s' % (fname, lname))
+ #   
+#hello('Victor', 'Mwai')
+
+
+#arithmetic Function
+
+#def deduction(pay, nhif):
+  #  pay_month = pay - nhif
+ #   print(pay_month)
+    
+#deduction(500 , 10)
+
+#using return.
+def deduction(pay, nhif):
+    return pay - nhif
+    
+print(deduction(500 , 10))
