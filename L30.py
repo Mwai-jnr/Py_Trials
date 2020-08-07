ec7679ad02f38e57be0cae5cf2a1f1887a06705b
vcom254 <mwaivictor07@yahoo.com>
2020-08-07 21:17:47 +0300

    lesson 30

---
+++ L30.py
@@ -0,0 +1,41 @@
+# combining conditions
+#using or or and
+age = 15
+
+# using or
+
+if age == 13 or age == 14 or age == 15 or age == 16 or age == 17 or age == 18 or age == 19:
+    print('you are a teenager!')
+else:
+    print ('you are not a teenager....')
+    
+# using and
+
+if age > 12 and age < 20 :
+    print('You are a teenager....')
+else:
+    print('you are not a teenager!.....')
+    
+    
+# when we use a single = sign it's known as assing
+# while when we use two == signs its equality.
+#ie
+
+Age = 23
+gender = 'male' # we have to usee parenthesisi sinces its a string.
+
+if age == 23 and gender == 'male': #using equality.... still use quoatation marks since its a string.
+    print ('you are old and male')
+else:
+    print('you are not old and male')
+    
+    
+#using assign
+
+#age =23
+#gender = 'male'
+
+#if age =23 and gender = 'male': #using a assign which will bringa bout an error in the code.
+ #  print ('you are old and male')
+#else:
+ #   print('you are not old and male')
